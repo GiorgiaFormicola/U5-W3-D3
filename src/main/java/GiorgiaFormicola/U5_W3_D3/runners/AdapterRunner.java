325955f69e2231ee72124eb5eb2c6d9f27a877b8
GiorgiaFormicola <giorgia.formicola97@gmail.com>
2026-04-22 14:46:10 +0200

    Updates AdapterRunner

--- src/main/java/GiorgiaFormicola/U5_W3_D3/runners/AdapterRunner.java
+++ src/main/java/GiorgiaFormicola/U5_W3_D3/runners/AdapterRunner.java
@@ -12,6 +12,8 @@ import java.text.SimpleDateFormat;
 public class AdapterRunner implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
+        System.out.println("\n------------- INIZIO ESERCIZIO ADAPTER -------------");
+
         UserData userData = new UserData();
 
         Info userInfo = new Info();
@@ -19,17 +21,19 @@ public class AdapterRunner implements CommandLineRunner {
         userInfo.setCognome("Rossi");
         userInfo.setDataDiNascita(new SimpleDateFormat("dd/MM/yyyy").parse("12/08/1997"));
 
-        System.out.println("\n--------- USER INFO ---------");
+        System.out.println("\n>>>>> USER INFO");
         System.out.println("NOME: " + userInfo.getNome());
         System.out.println("COGNOME: " + userInfo.getCognome());
         System.out.println("DATA DI NASCITA: " + userInfo.getDataDiNascita());
-        
+
         InfoAdapter userAdaptedInfo = new InfoAdapter(userInfo);
 
-        System.out.println("\n--------- USER INFO CONVERTITE A DATA SOURCE ---------");
+        System.out.println("\n>>>>> USER INFO CONVERTITE A DATA SOURCE");
         System.out.println("NOME COMPLETO: " + userAdaptedInfo.getNomeCompleto());
         System.out.println("ETA': " + userAdaptedInfo.getEtà());
 
         userData.getData(userAdaptedInfo);
+
+        System.out.println("\n------------- FINE ESERCIZIO ADAPTER -------------");
     }
 }
